f2dcb99d4c449c3dd5837be906d66976f785d4b4
hummai <shining.melon2015@gmail.com>
2020-03-26 13:41:22 +0200

    Added servlet and controller for products

---
+++ src/main/java/ua/lviv/lgs/servlets/ProductController.java
@@ -0,0 +1,27 @@
+package ua.lviv.lgs.servlets;
+
+import com.google.gson.Gson;
+import ua.lviv.lgs.entities.Product;
+import ua.lviv.lgs.services.ProductService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/api/product")
+public class ProductController extends HttpServlet {
+    ProductService productService = ProductService.getInstance();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Product> products = productService.getAll();
+        String jsonArray = new Gson().toJson(products);
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        resp.getWriter().write(jsonArray);
+    }
+}
